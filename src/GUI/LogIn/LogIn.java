0d5b52848e210c6f9801dea8cd1dc43a06a855c6
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-06-01 22:58:57 +0300

    Finalize Login functionality with correct paths and MainMenu restored

--- src/GUI/LogIn/LogIn.java
+++ src/GUI/LogIn/LogIn.java
@@ -2,11 +2,48 @@ package GUI.LogIn;
 
 import javax.swing.*;
 
-public class LogIn {
+
+public class LogIn extends JFrame {
     private JPanel panel1;
     private JTextField usernameTextField;
     private JPasswordField passwordTextField;
     private JLabel passwordLabel;
     private JLabel usernameLabel;
-    private JButton logInButton;
+    private JButton loginButton;
+    private JPanel authorization;
+    private JTabbedPane tabbedPane1;
+    private JTabbedPane LoginMenu;
+    private JRadioButton lightRadioButton;
+    private JButton fillButton;
+    private JButton a169Button;
+    private JRadioButton darkRadioButton;
+    private JTabbedPane ModeRadioButton;
+    private JTabbedPane SettingsLogIn;
+    private JTabbedPane WindowLogIn;
+    private JTabbedPane HelpLogIn;
+    private JPanel contactHelpLogin;
+    private JTabbedPane guideHelpLogIn;
+
+    public LogIn(){
+
+        setTitle("RetailHub - Login page");
+        setContentPane(panel1);
+        setDefaultCloseOperation(EXIT_ON_CLOSE);
+        pack();
+        setLocationRelativeTo(null);
+        setResizable(false);
+        setSize(600,300);
+
+        ButtonGroup radioMode = new ButtonGroup();
+        radioMode.add(darkRadioButton);
+        radioMode.add(lightRadioButton);
+
+
+        setVisible(true);
+
+    }
+
+
+
+
 }
--- src/Main.java
+++ src/Main.java
@@ -1,7 +1,9 @@
 import Connector.SQLiteConnector;
 import DAO.CustomerDAO;
 import DAO.ProductDAO;
-import GUI.MainMenu.MainMenu;
+import GUI.Customer.CustomerGUI;
+import GUI.Product.ProductGUI;
+import GUI.LogIn.LogIn;
 import Services.CustomerService;
 import Services.ProductService;
 
@@ -9,10 +11,8 @@ import Services.ProductService;
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 public class Main {
     public static void main(String[] args) {
-
         SQLiteConnector.initializeDatabase();
 
-        /**
         ProductDAO productDAO = new ProductDAO();
         ProductService productService = new ProductService(productDAO);
         new ProductGUI(productService);
@@ -20,14 +20,8 @@ public class Main {
         CustomerDAO customerDAO = new CustomerDAO();
         CustomerService customerService = new CustomerService(customerDAO);
         new CustomerGUI(customerService);
-         **/
 
-        ProductDAO productDAO = new ProductDAO();
-        ProductService productService = new ProductService(productDAO);
-        CustomerDAO customerDAO = new CustomerDAO();
-        CustomerService customerService = new CustomerService(customerDAO);
-        MainMenu menu1 = new MainMenu(productService, customerService);
-
+        LogIn login = new LogIn();
 
 
 

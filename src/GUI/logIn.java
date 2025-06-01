5a85dad1eece9fc4ec474cf842f0106f9fcafe22
IliasMaz <162236667+IliasMaz@users.noreply.github.com>
2025-06-01 19:29:02 +0300

    Initialization of LogIn

---
+++ src/GUI/logIn.java
@@ -0,0 +1,11 @@
+package GUI;
+
+import javax.swing.*;
+
+public class MainMenu {
+    private JPanel panel1;
+    private JTextField usernameTextField;
+    private JPasswordField passwordTextField;
+    private JLabel passwordLabel;
+    private JLabel usernameLabel;
+}
